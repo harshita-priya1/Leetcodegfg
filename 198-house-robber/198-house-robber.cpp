70e4120bd7859bdcbe5a303d22ad37e2a89637f8
harshita-priya1 <88089460+harshita-priya1@users.noreply.github.com>
2022-06-25 23:44:24 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (46.47%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -7,9 +7,9 @@ public:
         dp[n]=0;
         dp[n+1]=0;
         for(int i=n-1;i>=0;i--){
-            int sum1=dp[i+1];
-            int sum2=nums[i]+dp[i+2];
-            dp[i]=max(sum1,sum2);
+            int suma=dp[i+1];
+            int sumb=nums[i]+dp[i+2];
+            dp[i]=max(suma,sumb);
         }
         return dp[0];
         
