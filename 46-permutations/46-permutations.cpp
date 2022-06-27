3fd2ff5b89542d76815438115914510d0ce6acee
harshita-priya1 <88089460+harshita-priya1@users.noreply.github.com>
2022-06-27 23:51:48 +0530

    Time: 27 ms (5.03%), Space: 10.5 MB (5.21%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        if(nums.size()<=1) return {nums};
+        vector<vector<int>> result;
+        for(int i=0;i<nums.size();++i){
+            vector<int>v(nums.begin(),nums.end());
+            v.erase(v.begin()+i);
+            auto res=permute(v);
+            for(int j=0;j<res.size();++j){
+                vector<int> v1=res[j];
+                v1.insert(v1.begin(), nums[i]);
+                result.push_back(v1);
+            }
+        }
+        return result;
+    }
+};
