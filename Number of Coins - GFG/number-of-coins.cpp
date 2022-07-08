4858323366c9286b8701e71a7c0ed72fa16a7eba
harshita-priya1 <88089460+harshita-priya1@users.noreply.github.com>
2022-07-09 02:34:45 +0530

    Added solution - LeetHub

--- Number of Coins - GFG/number-of-coins.cpp
+++ Number of Coins - GFG/number-of-coins.cpp
@@ -21,9 +21,29 @@ class Solution{
 	}
 	int minCoins(int coins[], int m, int v) 
 	{ 
-	    
-	    vector<vector<int>> dp(m,vector<int>(v+1,-1));
-	    int ans=solve(coins,dp,v,m-1);
+	    if(m==1){
+	        if(v%coins[0]==0) return v/coins[0];
+	        else return -1;
+	    }
+	    vector<vector<int>> dp(m+1,vector<int>(v+1,0));
+	    for(int i=0;i<=v;i++){
+	        dp[0][i]=1e9;
+	    }
+	    for(int i=0;i<m+1;i++){
+	        dp[i][0]=0;
+	    }
+	    for(int i=1;i<=m;i++){
+	        for(int j=1;j<=v;j++){
+	            int t,nt;
+	            if(coins[i-1]<=j) t=1+dp[i][j-coins[i-1]];
+        	    else{
+        	        t= 1e9;
+        	    }
+        	    nt= dp[i-1][j];
+        	    dp[i][j]=min(t,nt);
+	        }
+	    }
+	    int ans=dp[m][v];
 	    if(ans==1e9) return -1;
 	    return ans;
 	    
