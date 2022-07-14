d63605af059a0eb34b7d3ce06169d7d5fee72296
harshita-priya1 <88089460+harshita-priya1@users.noreply.github.com>
2022-07-14 18:08:57 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -10,36 +10,31 @@ using namespace std;
 
 class Solution{
     public:
-    void solve(vector<vector<int>> &m, vector<string>& ans, vector<vector<int>>& vis,string dir, int n, int i, int j){
+    void solve(vector<vector<int>> &m, vector<string>& ans, 
+    vector<vector<int>>& vis,string dir, int n, int i, int j, int di[], 
+    int dj[]){
         if(i==n-1 && j==n-1){
             ans.push_back(dir);
             return;
         }
-        if(i+1<n && !vis[i+1][j] && m[i+1][j]==1){
-            vis[i][j]=1;
-            solve(m,ans,vis,dir+'D',n,i+1,j);
-            vis[i][j]=0;
-        }
-        if(j-1>=0 && !vis[i][j-1] && m[i][j-1]==1){
-            vis[i][j]=1;
-            solve(m,ans,vis,dir+'L',n,i,j-1);
-            vis[i][j]=0;
-        }
-        if(j+1<n && !vis[i][j+1] && m[i][j+1]==1){
-            vis[i][j]=1;
-            solve(m,ans,vis,dir+'R',n,i,j+1);
-            vis[i][j]=0;
-        }
-        if(i-1>=0 && !vis[i-1][j] && m[i-1][j]==1){
-            vis[i][j]=1;
-            solve(m,ans,vis,dir+'U',n,i-1,j);
-            vis[i][j]=0;
+        string s="DLRU";
+        for(int ind=0;ind<4;ind++){
+            int nexti=i +di[ind];
+            int nextj=j +dj[ind];
+            if(nexti>=0 && nexti<n && nextj>=0 && nextj<n && !vis[nexti][nextj]
+            && m[nexti][nextj]==1){
+                vis[i][j]=1;
+                solve(m,ans,vis,dir+s[ind],n,nexti,nextj,di,dj);
+                vis[i][j]=0;
+            }
         }
     }
     vector<string> findPath(vector<vector<int>> &m, int n) {
         vector<string> ans;
         vector<vector<int>> vis(n,vector<int>(n,0));
-        if(m[0][0]==1) solve(m,ans,vis,"",n,0,0);
+        int di[]={1,0,0,-1};
+        int dj[]={0,-1,1,0};
+        if(m[0][0]==1) solve(m,ans,vis,"",n,0,0,di,dj);
         return ans;
         // Your code goes here
     }
