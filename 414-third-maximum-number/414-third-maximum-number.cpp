9cb66c0391ada871b322bfb15ae8fd88a466f6b9
harshita-priya1 <88089460+harshita-priya1@users.noreply.github.com>
2022-07-27 00:32:10 +0530

    Time: 15 ms (33.83%), Space: 10.6 MB (33.54%) - LeetHub

---
+++ 414-third-maximum-number/414-third-maximum-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    
+    int thirdMax(vector<int>& nums) {
+        set<int> s;
+        for(int i: nums){
+            s.insert(i);
+            if(s.size()>3) s.erase(s.begin());
+        }
+        return s.size()==3 ? *s.begin() : *s.rbegin();
+    }
+};
