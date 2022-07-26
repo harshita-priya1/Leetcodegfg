3537ab459d0d8a9457dfee82332ee78311cee01e
harshita-priya1 <88089460+harshita-priya1@users.noreply.github.com>
2022-07-27 01:26:02 +0530

    Time: 7 ms (29.73%), Space: 10.4 MB (9.73%) - LeetHub

--- 1037-valid-boomerang/1037-valid-boomerang.cpp
+++ 1037-valid-boomerang/1037-valid-boomerang.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
-    bool isBoomerang(vector<vector<int>>& p) {
-        return p[0][0] * (p[1][1] - p[2][1]) + p[1][0] * (p[2][1] - p[0][1]) + p[2][0] * (p[0][1] - p[1][1]) != 0;
+    bool isBoomerang(vector<vector<int>>& points) {
+        if(points.size() != 3)
+			return false;
+        return ((points[1][1] - points[0][1]) * (points[2][0] - points[0][0])) != ((points[2][1] - points[0][1]) * (points[1][0] - points[0][0]));
     }
 };
