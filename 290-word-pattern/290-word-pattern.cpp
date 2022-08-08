d885535c4f0d5f06be60e502ae93547aa8e786a1
harshita-priya1 <88089460+harshita-priya1@users.noreply.github.com>
2022-08-08 23:34:26 +0530

    Time: 3 ms (44.13%), Space: 6.3 MB (95.94%) - LeetHub

--- 290-word-pattern/290-word-pattern.cpp
+++ 290-word-pattern/290-word-pattern.cpp
@@ -4,12 +4,12 @@ public:
         map<char, int> pi;
         map<string, int> wi;
         istringstream in(s);
-        int i = 0, n = pat.size();
-        for (string w; in >> w; ++i) {
-            if (i == n || pi[pat[i]] != wi[w])
+        int j = 0, n = pat.size();
+        for (string w; in >> w; ++j) {
+            if (j == n || pi[pat[j]] != wi[w])
                 return false;
-            pi[pat[i]] = wi[w] = i + 1;
+            pi[pat[j]] = wi[w] = j + 1;
         }
-        return i == n;
+        return j == n;
     }
 };
