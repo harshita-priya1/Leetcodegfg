e6e599c998fedb22b05e300d9f2b05223fbbe036
harshita-priya1 <88089460+harshita-priya1@users.noreply.github.com>
2022-08-25 20:07:29 +0530

    Time: 6 ms (16.67%), Space: 6 MB (15.67%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -5,9 +5,10 @@ public:
         if(n==1) return 0;
         if(dp[n]!=-1) return dp[n];
         else{
-            for(int x = 1;x<n;x++){
+            for(int x = 1;x*x<=n;x++){
                 if(n%x==0){
                     if(solve(n-x)==0) return dp[n]=1;
+                    if(x!=1 && solve(n-(n/x))==0) return dp[n]=1;
                 }
             }
             return dp[n]=0;
