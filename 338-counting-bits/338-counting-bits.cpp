54d800d1328a16fe04bf3fce06a038fcf158a595
harshita-priya1 <88089460+harshita-priya1@users.noreply.github.com>
2022-08-30 23:37:11 +0530

    Time: 12 ms (32.02%), Space: 7.8 MB (87.51%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+    vector<int> countBits(int num) {
+        //if you keenly observe 
+        //we can use what we have already calculated
+        //if a number is 8 then the number of set bits in it would be the number of set bits in half of its number ie., 4
+        //since 8 - 100
+        //      4-  010
+        //when we right shift the 8 by 1 we are only gonna lose the last bit 
+        //and remaining set bits are same 
+        vector<int> res(num+1);
+        res[0]=0;
+        for(int i=1;i<=num;i++)
+        {
+            res[i]= (i&1) + res[i>>1]; //if i is odd we will have set bit at last as we gonna lose it (ex- 101 )
+        }
+        return res;
+    }
+};
