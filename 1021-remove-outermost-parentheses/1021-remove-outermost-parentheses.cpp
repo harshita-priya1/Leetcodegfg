3004de7474cc3d3d59f0fa1c8bbf944633e6224b
harshita-priya1 <88089460+harshita-priya1@users.noreply.github.com>
2022-09-04 00:33:17 +0530

    Time: 4 ms (66.99%), Space: 6.6 MB (80.92%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        stack <char> st;
-        int n=s.size(),i=0;
-        string str, temp;
-        while(i<n){
-            char c=s[i++];
-            if(c=='('){
-                st.push(c);
-                temp.push_back(c);
-            }
-            else{
-                st.pop();
-                temp.push_back(c);
-            }
-            if(st.empty()){
-                str+=temp.substr(1,temp.size()-2);
-                temp="";
+        int count = 0;
+        string str;
+        for (char c : s) {
+            if (c == '(') {
+                if (count++) {
+                    str += '(';
+                }
+            } else {
+                if (--count) {
+                    str += ')';
+                }
             }
         }
         return str;
