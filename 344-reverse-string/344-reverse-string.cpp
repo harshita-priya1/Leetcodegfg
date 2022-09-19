1b8d83fd08ff8caf4b4828d5eba562524e98ecec
harshita-priya1 <88089460+harshita-priya1@users.noreply.github.com>
2022-09-20 00:34:59 +0530

    Time: 51 ms (20.64%), Space: 23.1 MB (93.67%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
-    void solve(vector<char>& s,int l,int r){
-        if(l>=r) return;
-        swap(s[l++],s[r--]);
-        solve(s,l,r);
-    }
-    
     void reverseString(vector<char>& s) {
-        solve(s,0,s.size()-1);
+        int i=0,j=s.size()-1;
+        while(i<j){
+            swap(s[i++],s[j--]);
+        }
     }
 };
